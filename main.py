ef5fd0721580181327e9bc239c8ff695e0dd423c
Senashu <118660067+Senashu@users.noreply.github.com>
2023-04-12 12:02:10 +0200

    now waits if ty crash

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 import os
 import time
+
 from pymem import Pymem
 from pymem.process import module_from_name
+
+game_choice = input("Enter 1 for TY or 2 for Mul-Ty-Player: ")
+
+if game_choice == "1":
+    exe_name = "TY.exe"
+elif game_choice == "2":
+    exe_name = "Mul-Ty-Player.exe"
+else:
+    print("Invalid game choice entered.")
+    exit()
 
 
 class TyReader:
     def __init__(self):
-        self.mem = Pymem("Mul-Ty-Player.exe")
-        self.module = module_from_name(self.mem.process_handle, "Mul-Ty-Player.exe").lpBaseOfDll
-        self.Cogs = self.module + 0x265260
-        self.bilby = self.module + 0x2651AC
-        self.opals = self.module + 0x2888B0
+        self.mem = None
+        self.module = None
+        self.Cogs = None
+        self.bilby = None
+        self.opals = None
+
+    def open_process(self):
+        try:
+            self.mem = Pymem(exe_name)
+            self.module = module_from_name(self.mem.process_handle, exe_name).lpBaseOfDll
+            self.Cogs = self.module + 0x265260
+            self.bilby = self.module + 0x2651AC
+            self.opals = self.module + 0x2888B0
+            return True
+        except:
+            return False
 
     def update_values(self, te_value=72):
         while True:
+            while not self.open_process():
+                print("Game process not found. Waiting for it to start...")
+                time.sleep(5)
+
             directory = r'C:\Program Files (x86)\Steam\userdata\80222993\411960\remote'
             filename = 'Game 2'
             file_path = os.path.join(directory, filename)
@@ -25,7 +51,6 @@ class TyReader:
             hex_14th = hex_14th[2:]
 
             TE = int(hex_14th, 16)
-
             Cogs = self.mem.read_int(self.Cogs)
             bilby = self.mem.read_int(self.bilby)
             opals = self.mem.read_int(self.opals)
